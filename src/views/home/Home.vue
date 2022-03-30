b5c7571a406353c1b3416a3b9c0c0741b0db1fcc
wazty_zdj <5999552@qq.com>
2022-03-30 14:31:09 +0800

    导航栏组件封装及首页导航栏实现

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav-bar">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navBar/NavBar'
+
 export default {
   name: "Home",
+  components: {
+    NavBar
+  }
 };
 </script>
 
 <style>
+  .home-nav-bar {
+    text-align: center;
+    background: var(--color-tint);
+    color: #fff;
+  }
 </style>
