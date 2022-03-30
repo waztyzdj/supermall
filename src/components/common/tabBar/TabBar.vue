a8a6077334cc04c45464b7eeb7d574be35c135e0
wazty_zdj <5999552@qq.com>
2022-03-30 12:30:49 +0800

    安装路由及配置，并划分项目模块。

---
+++ src/components/common/tabBar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="tab-bar">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  display: flex;
+  background-color: #f2f2f2;
+
+  position: fixed;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+
+  box-shadow: 0px -1px 1px rgba(100, 100, 100, 0.1);
+}
+</style>
---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,59 @@
+<template>
+    <tab-bar>
+      <tab-bar-item v-for="item in tabBarItemList" :path="item.path" :activeColor="item.activeColor" :key="item.path">
+        <img slot="item-icon" :src="item.icon" alt="" />
+        <img slot="item-icon-active" :src="item.iconActive" alt="" />
+        <div slot="item-text">{{item.text}}</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/common/tabBar/TabBar.vue";
+import TabBarItem from "components/common/tabBar/TabBarItem.vue";
+export default {
+  name: "MainTabBar",
+  data() {
+    return {
+      tabBarItemList: [
+        {
+          text: '首页',
+          path: '/home',
+          icon: '/img/tabbar/home.svg',
+          iconActive: '/img/tabbar/home_active.svg',
+          activeColor: 'deeppink'
+        },
+        {
+          text: '分类',
+          path: '/category',
+          icon: '/img/tabbar/category.svg',
+          iconActive: '/img/tabbar/category_active.svg',
+          activeColor: 'deeppink'
+        },
+        {
+          text: '购物车',
+          path: '/cart',
+          icon: '/img/tabbar/cart.svg',
+          iconActive: '/img/tabbar/cart_active.svg',
+          activeColor: 'deeppink'
+        },
+        {
+          text: '我的',
+          path: '/profile',
+          icon: '/img/tabbar/profile.svg',
+          iconActive: '/img/tabbar/profile_active.svg',
+          activeColor: 'deeppink'
+        }
+      ]
+    }
+  },
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
