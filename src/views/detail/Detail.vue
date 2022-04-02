652dbf8b27f732a4dfcc026c246b9eab1d9c44f7
unknown <wazty_zdj@163.com>
2022-04-02 13:21:48 +0800

    详情页轮播图实现。

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="detail">
     <detail-nav-bar/>
+    <detail-swiper :topImages="topImages" class="detail-swiper"></detail-swiper>
   </div>
 </template>
 
 <script>
-import DetailNavBar from "views/detail/childComponents/DetailNavBar";
+import DetailNavBar from "./childComponents/DetailNavBar";
+import DetailSwiper from "./childComponents/DetailSwiper";
 
 import { getDetail } from "network/detail";
 
@@ -13,13 +15,15 @@ export default {
   name: "Detail",
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: []
     }
   },
   components: {
-    DetailNavBar
+    DetailNavBar,
+    DetailSwiper
   },
-  activated() {
+  created() {
     this.iid = this.$route.params.iid
 
     // 请求数据
@@ -29,6 +33,7 @@ export default {
     getDetailData() {
       getDetail(this.iid).then(res => {
         console.log(res)
+        this.topImages = res.result.itemInfo.topImages
       })
     }
   }
@@ -36,5 +41,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .detail-swiper {
+    height: 300px;
+  }
 </style>
---
+++ src/views/detail/childComponents/DetailSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in topImages" :key="item">
+      <a :href="item">
+        <img :src="item">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from 'components/common/swiper'
+
+export default {
+  name: "DetailSwiper",
+  props: {
+    topImages: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
